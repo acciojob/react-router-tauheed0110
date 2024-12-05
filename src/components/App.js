b619f2830a5ab82bda5d6ab0c2e284783b6b5b2c
saif0110 <khanmohdtauheed4@gmail.com>
2024-12-05 15:27:03 +0530

    file changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <BrowserRouter>
+          <nav>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+          </nav>
+          <Routes>
+            <Route path="/" element={<h1>Welcome to my website!</h1>}/>
+            <Route path="/about" element={<About />}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
 
 export default App
+
+function About(){
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a sample React Router program.</p>
+    </div>
+  )
+}
